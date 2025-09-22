e0053693f39c7e540134f65981a931a6945a792d
hidayat <hidayathamir@gmail.com>
2025-09-22 22:53:33 +0700

    main.go print go to swagger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -34,6 +34,7 @@ func main() {
 	})
 
 	webPort := viperConfig.GetInt(configkey.WebPort)
+	fmt.Printf("Go to swagger http://localhost:%d/swagger\n", webPort)
 	err := app.Listen(fmt.Sprintf(":%d", webPort))
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
